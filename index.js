90228323d81aaecd16c87bf5b932c2a00816242e
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-04-24 13:45:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
+    var fano = window.document.querySelector('input#txtano')
+    var res = window.document.getElementById('res')
+    if(fano.value == 0 || fano.value  > ano){
+        alert('[ERRO] Ano inválido, tente novamente!')
+    } else {
+        alert('Tudo ok!')
+    }
+    
     
 }
-alert(`${ano}`)
