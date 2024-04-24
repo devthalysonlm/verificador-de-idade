c346c2954fb4d8ae2e80f00a5f3d5291ba987d49
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-04-24 19:02:29 -0300

    Adicionando funcionalidade

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = window.document.querySelector('input#txtano')
     var res = window.document.getElementById('res')
-    if(fano.value == 0 || fano.value  > ano){
-        alert('[ERRO] Ano inválido, tente novamente!')
+    if(fano.value.lenght == 0 || fano.value  > ano){
+        window.alert('[ERRO] Ano inválido, tente novamente!')
     } else {
-        alert('Tudo ok!')
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //var img = coloque a imagem aqui depois de verificar o ano 
+        var genero = ''
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade < 12){
+                //Criança
+            } else if(idade < 18 ){
+                //Adolescente
+            } else if (idade < 50){
+                //Adulto
+            } else {
+                //Idoso
+            }
+
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Identificado ${genero} com ${idade} anos`
     }
     
     
